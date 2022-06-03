e71991f5f4517df436ce61b793ba90133fac6a1a
AmandaRichards <Richards.amanda92@gmail.com>
2022-06-03 02:17:50 +0100

    z index sorted

--- portfolio/src/components/header/index.js
+++ portfolio/src/components/header/index.js
@@ -4,9 +4,9 @@ import '../../styles/header.css'
 export const Header = () => {
   return (
       <div className='header'>
-    <h1>Hi, I'm Amanda</h1>
+    <h1>Hi,  I'm Amanda</h1>
     <br>
-        
+
     </br>
     <h2>Full stack software engineer</h2>
     </div>
