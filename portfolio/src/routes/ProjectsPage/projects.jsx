53c00d3e4acb71f92165b828a52302ba4e509093
AmandaRichards <Richards.amanda92@gmail.com>
2022-06-23 23:02:37 +0100

    projects mosaic layout

--- portfolio/src/routes/ProjectsPage/projects.jsx
+++ portfolio/src/routes/ProjectsPage/projects.jsx
@@ -1,31 +1,33 @@
 import React from 'react'
 import BurgerMenu from '../../animations/burgerMenu'
-import Card from '../../components/Card'
+// import Card from '../../components/Card'
 import css from './index.module.scss'
-import amanda from '../../Assets/Amanda.svg'
-import wastenot from '../../Assets/supermarket.png'
-import resource from '../../Assets/resource.png'
-import resources from '../../Assets/resources.png'
-import daire from '../../Assets/Daire.png'
+
 
 function Projects() {
   return (
-    <div>
-    <BurgerMenu />
-    <h1 className={css.h1}>WORK.</h1>
-    <div className={css.cardContainer}>
-       
-      <Card image={wastenot} title={`WasteNot: WantNot`}/>
-     <Card image={resources} />
-     </div>
-      <div className={css.cardContainer2}>
-      <Card image={daire}/>
-      <Card image={wastenot} />
-      <Card image={resource} />
-      <Card image={amanda}/>
-    </div>
-    </div>
+    
+      <><BurgerMenu /><h1 className={css.h1}>WORK.</h1><div className={css.cardContainer}>
+<div className={css.box1} ></div>
+<div className={css.box2} ></div>
+<div className={css.box3} ></div>
+<div className={css.box4} ></div>
+<div className={css.box5} ></div>
+<div className={css.box6} ></div>
+<div className={css.box7} ></div>
+
+
+
+
+
+
+      </div>
+
+
+ 
+    </>
   )
 }
 
 export default Projects
+
